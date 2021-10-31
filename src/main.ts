3ad3c149b27b19383f08f5333af7c2fd212cc4f5
Hafidiy <hafidiyalfayed@gmail.com>
2021-10-31 18:48:25 +0500

    changess

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { ChangeResponseInterceptor } from './modules/common/change-response.interceptor';
+import * as cookieParser from 'cookie-parser';
+import * as hbs from 'hbs';
+import * as exphbs from 'express-handlebars';
+import { join } from 'path';
+import { hbsConfig } from './config/engine.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalInterceptors(new ChangeResponseInterceptor());
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+
+  app.engine('hbs', hbsConfig.engine);
+  app.set('view engine', 'hbs');
+
+  app.use(cookieParser());
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
+  const PORT = process.env.PORT || 4000;
+  await app.listen(PORT);
 }
 bootstrap();
